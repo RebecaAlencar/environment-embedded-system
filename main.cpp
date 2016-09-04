0e1291085ccadd7b73393544f24a070815d9207c
Rebeca Alencar <rvsa@cin.ufpe.br>
2016-09-04 12:54:44 -0300

    Initial commit for main.cpp

---
+++ main.cpp
@@ -0,0 +1,39 @@
+#include <cstdio>
+#include <iostream>
+#include <string>
+#include <list>
+#include <algorithm>   
+#include <iterator> 
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include <iostream>
+#include <fstream>
+#include <boost/algorithm/string.hpp>
+#include <string>
+
+#include <xercesc/parsers/XercesDOMParser.hpp>
+#include <xercesc/dom/DOM.hpp>
+#include <xercesc/sax/HandlerBase.hpp>
+
+#include "Module.hpp"
+#include "Attribute.hpp"
+#include "Method.hpp"
+#include "Parameter.hpp"
+#include "Service.hpp"
+#include "Transaction.hpp"
+#include "Element.hpp"
+#include "Restriction.hpp"
+#include "Dependency.hpp"
+#include "Frame.hpp"
+#include "Device.hpp"
+#include "Artifact.hpp"
+#include "Component.hpp"
+#include "ExecutionEnvironment.hpp"
+#include "ParserErrorHandler.hpp"
+
+
+int main(){
+
+return 0;
+}
